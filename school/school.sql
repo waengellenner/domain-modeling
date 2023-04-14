4d92a1629abba722d4557ff3c90af77a900a410f
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-14 19:27:43 +0000

    Exercise school.sql

--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,28 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id integer,
+  teacher_id integer
+);
+
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  students_id integer,
+  sections_id integer
+)
